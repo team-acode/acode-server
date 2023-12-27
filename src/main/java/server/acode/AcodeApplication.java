c5b949d1309bfee0336886a969b5abb46d5cad52
‘yj-leez’ <yj_leez@naver.com>
2023-12-27 23:42:23 +0900

    setting: 초기 세팅

---
+++ src/main/java/server/acode/AcodeApplication.java
@@ -0,0 +1,13 @@
+package server.acode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/acode/AcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package server.acode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
