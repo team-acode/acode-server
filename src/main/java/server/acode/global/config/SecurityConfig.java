fd232cb1e84c9fef4560481f6a5378ccc111ad8a
‘yj-leez’ <yj_leez@naver.com>
2024-01-02 18:28:18 +0900

    feat: config 수정

--- src/main/java/server/acode/global/config/SecurityConfig.java
+++ src/main/java/server/acode/global/config/SecurityConfig.java
@@ -4,22 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import server.acode.global.auth.jwt.JwtAuthenticationFilter;
-import server.acode.global.auth.jwt.JwtTokenProvider;
 import server.acode.global.auth.oauth.CustomOAuth2UserService;
 import server.acode.global.auth.oauth.OAuthSuccessHandler;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 
 
 @Configuration
@@ -52,28 +47,10 @@ public class SecurityConfig {
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
-                .formLogin(formLogin -> formLogin.disable()); // 사용자 지정 로그인 로직 구현
-//                .httpBasic(HttpBasicConfigurer::disable); // http 기본 인증 비활성화
+                .formLogin(formLogin -> formLogin.disable()) // 사용자 지정 로그인 로직 구현
+                .httpBasic(HttpBasicConfigurer::disable); // http 기본 인증 비활성화
 
         return http.build();
     }
 
-    @Bean
-    public AuthenticationSuccessHandler successHandler() {
-        return ((request, response, authentication) -> {
-            DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
-
-            String id = defaultOAuth2User.getAttributes().get("id").toString();
-            String body = """
-                    {"id":"%s"}
-                    """.formatted(id);
-
-            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-
-            PrintWriter writer = response.getWriter();
-            writer.println(body);
-            writer.flush();
-        });
-    }
 }
