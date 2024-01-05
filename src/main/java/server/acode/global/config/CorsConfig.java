166325291eedd71a21a5f942475690cc9cdd3985
‘yj-leez’ <yj_leez@naver.com>
2024-01-05 14:28:22 +0900

    feat: cors 설정 추가

---
+++ src/main/java/server/acode/global/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package server.acode.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        ArrayList<String> allowedOriginPatterns = new ArrayList<>();
+        allowedOriginPatterns.add("http://localhost:3000");
+        allowedOriginPatterns.add("http://localhost:8080");
+        allowedOriginPatterns.add("https://abcode.shop");
+        allowedOriginPatterns.add("https://abcode.shop/");
+        String[] patterns = allowedOriginPatterns.toArray(String[]::new);
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowedOriginPatterns(patterns)
+                .allowCredentials(true)
+                .maxAge(3600L);
+    }
+}
