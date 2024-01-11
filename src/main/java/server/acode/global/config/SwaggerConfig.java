92a01544d40d51fa83e7a2f69249cb30026fceff
‘yj-leez’ <yj_leez@naver.com>
2024-01-11 10:57:16 +0900

    fix: swagger 오류 수정

--- src/main/java/server/acode/global/config/SwaggerConfig.java
+++ src/main/java/server/acode/global/config/SwaggerConfig.java
@@ -12,39 +12,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
-//    @Bean
-//    public OpenAPI publicApi() {
-//        Server server = new Server().url("/");
-//
-//        return new OpenAPI()
-//                .info(getSwaggerInfo())
-//                .components(authSetting())
-//                .addServersItem(server);
-//    }
-//
-//    private Info getSwaggerInfo() {
-//        License license = new License();
-//        license.setName("Acode");
-//
-//        return new Info()
-//                .title("Acode API 명세서")
-//                .description("Acode API 명세서입니다")
-//                .version("v0.0.1")
-//                .license(license);
-//    }
-//
-//    private Components authSetting() {
-//        return new Components()
-//                .addSecuritySchemes(
-//                        "Authorization",
-//                        new SecurityScheme()
-//                                .type(SecurityScheme.Type.HTTP)
-//                                .scheme("bearer")
-//                                .bearerFormat("JWT")
-//                                .in(SecurityScheme.In.HEADER)
-//                                .name("Authorization"));
-//    }
     @Bean
     public OpenAPI openAPI() {
     Info info = new Info()
@@ -59,13 +26,14 @@ public class SwaggerConfig {
             .type(SecurityScheme.Type.HTTP)
             .scheme("bearer")
             .bearerFormat("JWT")
-    );
+        );
 
     return new OpenAPI()
+            .addServersItem(new Server().url("/"))
             .info(info)
             .addSecurityItem(securityRequirement)
             .components(components);
-}
+    }
 
 
 }
