81d2f7e4b092eac51817aee47f7b51aee726a782
yoonsseo <yellisa2000@gmail.com>
2024-01-12 19:51:21 +0900

    feat: 리뷰 등록 구현 #11

--- src/main/java/server/acode/domain/fragrance/repository/FragranceRepository.java
+++ src/main/java/server/acode/domain/fragrance/repository/FragranceRepository.java
@@ -1,9 +1,18 @@
 package server.acode.domain.fragrance.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import server.acode.domain.fragrance.entity.Fragrance;
 
 @Repository
 public interface FragranceRepository extends JpaRepository<Fragrance, Long> {
+    @Modifying
+    @Query("UPDATE Fragrance f " +
+            "SET f.reviewCnt = f.reviewCnt + 1, " +
+            "f.rateSum = f.rateSum + :rate " +
+            "WHERE f.id = :fragranceId")
+    void increaseReview(@Param("rate") int rate, @Param("fragranceId") Long fragranceId);
 }
---
+++ src/main/java/server/acode/domain/review/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package server.acode.domain.review.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import server.acode.domain.review.entity.Review;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
