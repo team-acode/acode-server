9571e4ae0c39a5fbfe1491d42469a29b2fba9de4
yoonsseo <yellisa2000@gmail.com>
2024-01-15 12:32:47 +0900

    feat: 상세 - 쿼리dsl 상속 추가 #7

--- src/main/java/server/acode/domain/family/repository/FragranceFamilyRepository.java
+++ src/main/java/server/acode/domain/family/repository/FragranceFamilyRepository.java
@@ -1,8 +1,6 @@
 package server.acode.domain.family.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import server.acode.domain.family.entity.Family;
 import server.acode.domain.family.entity.FragranceFamily;
@@ -11,7 +9,6 @@ import server.acode.domain.fragrance.entity.Fragrance;
 import java.util.List;
 
 @Repository
-public interface FragranceFamilyRepository extends JpaRepository<FragranceFamily, Long> {
-    @Query("SELECT ff.family FROM FragranceFamily ff WHERE ff.fragrance = :fragrance")
-    List<Family> findByFragrance(@Param("fragrance") Fragrance fragrance);
+public interface FragranceFamilyRepository extends JpaRepository<FragranceFamily, Long>, FragranceFamilyRepositoryCustom {
+    List<Family> findByFragrance(Fragrance fragrance);
 }
