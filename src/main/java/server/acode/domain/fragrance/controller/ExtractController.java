8a28f5c8d180801da7d416493d20f2836661f2a4
yoonsseo <yellisa2000@gmail.com>
2024-01-18 17:54:15 +0900

    feat: Swagger 적용 #23

--- src/main/java/server/acode/domain/fragrance/controller/ExtractController.java
+++ src/main/java/server/acode/domain/fragrance/controller/ExtractController.java
@@ -1,5 +1,7 @@
 package server.acode.domain.fragrance.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,9 +11,12 @@ import server.acode.domain.fragrance.service.ExtractService;
 @RestController
 @RequestMapping("/api/v1/extract")
 @RequiredArgsConstructor
+@Tag(name = "Extract", description = "매칭 테스트 API")
 public class ExtractController {
     private final ExtractService extractService;
 
+    @Operation(summary = "매칭 테스트",
+            description = "아직 미완성 입니다")    
     @GetMapping // 나중에 POST로ㅎ
     public ResponseEntity<?> extractFamily(@RequestBody KeywordCond cond) {
         return extractService.extractFamily(cond);
