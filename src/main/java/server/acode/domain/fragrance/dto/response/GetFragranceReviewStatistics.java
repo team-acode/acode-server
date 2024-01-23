a56d2c0d1ef059c6b8290b4c88fb624238f5ad29
yoonsseo <yellisa2000@gmail.com>
2024-01-24 04:37:18 +0900

    feat: 상세 - 리뷰 통계 수정 #7

--- src/main/java/server/acode/domain/fragrance/dto/response/GetFragranceReviewStatistics.java
+++ src/main/java/server/acode/domain/fragrance/dto/response/GetFragranceReviewStatistics.java
@@ -1,32 +1,17 @@
 package server.acode.domain.fragrance.dto.response;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class GetFragranceReviewStatistics {
-    private int reviewCnt;
-
-    private String season;
-    private int seasonMax;
-
-    private String longevity;
-    private int longevityMax;
-
-    private String intensity;
-    private int intensityMax;
-
-    private String style1;
-    private int styleMax1;
-
-    private String style2;
-    private int styleMax2;
-
-    private String style3;
-    private int styleMax3;
+    private List<ReviewStatInfo> seasonStat;
+    private List<ReviewStatInfo> longevityStat;
+    private List<ReviewStatInfo> intensityStat;
+    private List<ReviewStatInfo> styleStat;
 }
